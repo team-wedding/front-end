1a310dd9ad9aa8ecf0fcfa142e64342be8298c42
hcy08 <sweet17strawberry@gmail.com>
2024-12-16 21:15:26 +0900

    Feat: 사용자 연락처 입력 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import './App.css';
 import React from 'react';
-import ContactInput from './components/ContactInput';
 
 function App() {
-  return (
-    <>
-      <ContactInput />
-    </>
-  );
+  return <></>;
 }
 
 export default App;

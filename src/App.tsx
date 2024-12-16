b77a36dcd6778ad09fd4455fb7cf93c71e5a2f7c
hcy08 <sweet17strawberry@gmail.com>
2024-12-17 03:42:40 +0900

    Design: flowbite css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css';
 import React from 'react';
+import ContactInput from './components/ContactInput';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ContactInput />
+    </>
+  );
 }
 
 export default App;
--- src/components/ContactInput.tsx
+++ src/components/ContactInput.tsx
@@ -4,86 +4,52 @@ import useContactStore from './useContactStore';
 const ContactInput = () => {
   const contacts = useContactStore((state) => state.contacts);
   const updateContact = useContactStore((state) => state.updateContact);
-  const handleSubmit = () => {
-    alert('저장 완료');
-  };
 
   return (
     <div>
       {contacts.map((person, index) => (
-        <div key={index}>
-          <div>
-            <label>
-              {person.role}:
-              <input
-                type="tel"
-                placeholder="010-0000-0000"
-                value={person.contact}
-                onChange={(e) =>
-                  updateContact(index, 'contact', e.target.value)
-                }
-              />
-            </label>
-            {person.contact && (
-              <div>
-                <a href={`tel:${person.contact}`}>
-                  <button>전화</button>
-                </a>
-                <a href={`sms:${person.contact}`}>
-                  <button>문자</button>
-                </a>
-              </div>
-            )}
+        <div key={index} className="p-2">
+          <div className="flex items-center mb-2">
+            <div className="min-w-[70px] text-left font-semibold">
+              {person.role}
+            </div>
+            <input
+              type="tel"
+              placeholder="010-0000-0000"
+              value={person.contact}
+              onChange={(e) => updateContact(index, 'contact', e.target.value)}
+              pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-5 p-2"
+            />
           </div>
-          <div>
-            <label>
-              아버지:
-              <input
-                type="tel"
-                placeholder="010-0000-0000"
-                value={person.fatherContact}
-                onChange={(e) =>
-                  updateContact(index, 'fatherContact', e.target.value)
-                }
-              />
-            </label>
-            {person.fatherContact && (
-              <div>
-                <a href={`tel:${person.fatherContact}`}>
-                  <button>전화</button>
-                </a>
-                <a href={`sms:${person.fatherContact}`}>
-                  <button>문자</button>
-                </a>
-              </div>
-            )}
+          <div className="flex items-center mb-2">
+            <div className="min-w-[70px] text-left font-semibold">아버지</div>
+            <input
+              type="tel"
+              placeholder="010-0000-0000"
+              value={person.fatherContact}
+              onChange={(e) =>
+                updateContact(index, 'fatherContact', e.target.value)
+              }
+              pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-5 p-2"
+            />
           </div>
-          <div>
-            <label>
-              어머니:
-              <input
-                type="tel"
-                placeholder="010-0000-0000"
-                value={person.motherContact}
-                onChange={(e) =>
-                  updateContact(index, 'motherContact', e.target.value)
-                }
-              />
-            </label>
-            {person.motherContact && (
-              <div>
-                <a href={`tel:${person.motherContact}`}>
-                  <button>전화</button>
-                </a>
-                <a href={`sms:${person.motherContact}`}>
-                  <button>문자</button>
-                </a>
-              </div>
-            )}
+          <div className="flex items-center mb-2">
+            <div className="min-w-[70px] text-left font-semibold">어머니</div>
+            <input
+              type="tel"
+              placeholder="010-0000-0000"
+              value={person.fatherContact}
+              onChange={(e) =>
+                updateContact(index, 'fatherContact', e.target.value)
+              }
+              pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-5 p-2"
+            />
           </div>
         </div>
       ))}
-      <button onClick={handleSubmit}>저장하기</button>
     </div>
   );
 };

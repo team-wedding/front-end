5561871c2cde806c8a0f10e9f174f20bcd48451d
Soyeon Lee <pink731p@gmail.com>
2024-12-16 21:03:16 +0900

    Feat: 캘린더, 디데이 컴포넌트 생성

---
+++ src/components/wedding/Calendar.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useWeddingStore } from '../../store/store';
+import {
+  eachDayOfInterval,
+  endOfMonth,
+  endOfWeek,
+  format,
+  isSameDay,
+  isSameMonth,
+  startOfMonth,
+  startOfWeek,
+} from 'date-fns';
+
+const Calendar: React.FC = () => {
+  const { weddingDate } = useWeddingStore();
+  const currentDate = weddingDate || new Date();
+
+  const dayOfWeeks = ['일', '월', '화', '수', '목', '금', '토'];
+  const firstDayOfMonth = startOfMonth(currentDate);
+  const lastDayOfMonth = endOfMonth(currentDate);
+  const startDay = startOfWeek(firstDayOfMonth); // 첫 주 일요일
+  const endDay = endOfWeek(lastDayOfMonth); // 마지막 주 토요일
+  const allDays = eachDayOfInterval({ start: startDay, end: endDay });
+
+  return (
+    <div className="flex flex-col items-center justify-center text-sm">
+      <div>
+        <div className="grid grid-cols-7 p-2 gap-2 text-center">
+          {dayOfWeeks.map((day) => (
+            <div key={day} className={`${day === '일' && 'text-rose-400'}`}>
+              {day}
+            </div>
+          ))}
+        </div>
+        <div className="grid grid-cols-7 gap-2 text-center">
+          {allDays.map((day, index) => {
+            const isCurrentMonth = isSameMonth(day, currentDate);
+            const isSelectedDay = isSameDay(day, weddingDate || new Date());
+            return (
+              <div
+                key={index}
+                className={`p-2 ${isSelectedDay && 'bg-rose-400 text-white rounded-full'}  ${index % 7 === 0 && 'text-rose-400'} ${isCurrentMonth ? 'text-gray-900' : 'text-opacity-0 text-transparent'}`}
+              >
+                {format(day, 'd')}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;

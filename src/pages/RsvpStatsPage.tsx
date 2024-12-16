d773a74601da1092a5ab415eda74e35b5962fd2d
Soyeon Lee <pink731p@gmail.com>
2024-12-16 21:30:03 +0900

    Feat: 기본 페이지 생성

---
+++ src/pages/RsvpStatsPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import BackIcon from '../components/icons/BackIcon';
+import PageLayout from '../components/common/PageLayout';
+import HeaderButton from '../components/common/HeaderButton';
+
+const RsvpStatsPage = () => {
+  const handleBack = () => {
+    console.log('전으로 돌아가기');
+  };
+
+  return (
+    <PageLayout
+      title="참석여부 집계 요약"
+      leftButton={
+        <HeaderButton onClick={handleBack}>
+          <BackIcon />
+        </HeaderButton>
+      }
+    >
+      <p>참석여부 통계 페이지</p>
+    </PageLayout>
+  );
+};
+
+export default RsvpStatsPage;

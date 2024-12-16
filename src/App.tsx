121da1b95a94722ac5a9f4ce7c8c9dfd92765849
hcy08 <sweet17strawberry@gmail.com>
2024-12-16 22:51:58 +0900

    Remove: App.tsx 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import './App.css';
 import React from 'react';
-import ImageUpload from './components/ImageUpload';
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <ImageUpload />
-    </>
-  );
+  return <></>;
 }
 
 export default App;

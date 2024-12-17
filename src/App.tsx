dc69f1eb973391c43872cc07b8ea1370cb2d3cbd
hcy08 <sweet17strawberry@gmail.com>
2024-12-17 15:33:20 +0900

    Remove: App.tsx 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
 import './App.css';
-import ImageUpload from './components/ImageUpload';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <ImageUpload />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;

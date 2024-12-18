2616d861f2cde1910dd7a4cda81a69304bd48485
meteorqz6 <nys8765@naver.com>
2024-12-18 22:08:51 +0900

    Design: 제목/인사말 컴포넌트 디자인 수정

--- src/components/form/GreetingInput/GreetingModal.tsx
+++ src/components/form/GreetingInput/GreetingModal.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import useGreetingStore from '../../../store/useGreetingStore';
 
-const greetingSample = ['샘플 1', '샘플 2'];
+const greetingSample =
+  [
+    '저희 두 사람의 작은 만남이 사랑의 결실을 이루어 소중한 결혼식을 올리게 되었습니다. 평생 서로 귀하게 여기며 첫 마음 그대로 존중하고 배려하며 살겠습니다. 오로지 믿음과 사랑을 약속하는 날오셔서 축복해 주시면 더없는 기쁨으로 간직하겠습니다.',
+    '서로에게 행복을 주는 사람을 만났습니다. 웃는 모습이 너무나 예쁜 그 사람을 만났습니다. 배려하는 마음이 따뜻한 그 사람을 만났습니다. 운명처럼 만나게 된 우리의 인연 그 인연에 이끌려 이제 영원을 함께 약속하려 합니다. 저희의 하나 됨을 지켜보아 주시고 격려해 주시면 더없는 기쁨으로  간직하겠습니다.'];
 
 const GreetingModal = ({
   isOpen,
@@ -23,8 +26,9 @@ const GreetingModal = ({
     >
       <div className="relative w-full max-w-md p-4">
         <div className="bg-white rounded-lg shadow-md">
+
           <div className="flex items-center justify-between p-4 border-b rounded-t">
-            <h3 className="text-lg font-semibold text-gray-900">
+            <h3 className="text-base font-semibold text-gray-900">
               샘플 문구 선택
             </h3>
             <button
@@ -50,37 +54,34 @@ const GreetingModal = ({
               <span className="sr-only">닫기</span>
             </button>
           </div>
-          <div className="p-4 space-y-4">
-            <ul className="space-y-4">
-              {greetingSample.map((text, index) => (
-                <li
-                  key={index}
-                  className={`cursor-pointer p-5 rounded-lg border ${
-                    text === selectedSample
-                      ? 'border-black bg-gray-100'
-                      : 'border-gray-200 hover:bg-gray-100'
-                  }`}
-                  onClick={() => setSelectedSample(text)}
-                >
-                  {text}
-                </li>
-              ))}
-            </ul>
-            <div className="mt-4">
+
+          <div className="flex flex-col gap-4 p-4">
+            {greetingSample.map((text, index) => (
               <button
-                onClick={() => {
-                  if (selectedSample) {
-                    setGreeting(selectedSample);
-                    onClose();
-                  }
-                }}
-                className="bg-blue-700 text-white py-2 rounded-lg hover:bg-blue-800"
-                disabled={!selectedSample}
+                key={index}
+                className={`text-left cursor-pointer p-5 rounded-lg border w-full ${text === selectedSample
+                    ? 'border-black bg-gray-100'
+                    : 'border-gray-200 hover:bg-gray-100'
+                  }`}
+                onClick={() => setSelectedSample(text)}
               >
-                적용하기
+                {text}
               </button>
-            </div>
+            ))}
+            <button
+              onClick={() => {
+                if (selectedSample) {
+                  setGreeting(selectedSample);
+                  onClose();
+                }
+              }}
+              className="px-4 py-2 text-white bg-primary rounded-xl hover:bg-pink-600"
+              disabled={!selectedSample}
+            >
+              적용하기
+            </button>
           </div>
+
         </div>
       </div>
     </div>
--- src/pages/CreateInvitationPage1.tsx
+++ src/pages/CreateInvitationPage1.tsx
@@ -6,6 +6,8 @@ import { useInvitationStore } from '../store/useInvitaionStore';
 import { useNavigate } from 'react-router';
 import { Accordion } from '../components/common/CreateInvitation/Accordion';
 import { accordionData } from '../constants/accordionData';
+// import { Stepper } from '../components/common/CreateInvitation/Stepper';
+// import { StepNavigation } from '../components/common/CreateInvitation/StepNavigation';
 
 const CreateInvitationPage1: React.FC = () => {
   const { title, setTitle } = useInvitationStore();
@@ -15,13 +17,24 @@ const CreateInvitationPage1: React.FC = () => {
   const handleSave = () => console.log('저장 버튼 클릭, 제목: ', title);
 
   const [expandedIds, setExpandedIds] = useState<number[]>([]);
-  
+
   const toggleExpand = (id: number) => {
     setExpandedIds((prev) =>
       prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
     );
   };
 
+  // const steps = ["기본 정보 입력", "기능 선택", "테마 선택"];
+
+  // const [currentStep, setCurrentStep] = useState<number>(1);
+
+  // const handleNext = () => {
+  //   setCurrentStep((prev) => Math.min(prev + 1, steps.length));
+  // };
+
+  // const handlePrev = () => {
+  //   setCurrentStep((prev) => Math.max(prev - 1, 1));
+  // };
 
   return (
     <PageLayout
@@ -49,7 +62,14 @@ const CreateInvitationPage1: React.FC = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <Accordion items={accordionData} expandedIds={expandedIds} toggleExpand={toggleExpand} />
+        {/* <Stepper steps={steps} currentStep={currentStep} /> */}
+        <Accordion items={accordionData.slice(0, 6)} expandedIds={expandedIds} toggleExpand={toggleExpand} />
+        {/* <StepNavigation
+          currentStep={currentStep}
+          totalSteps={steps.length}
+          onPrev={handlePrev}
+          onNext={handleNext}
+        /> */}
       </div>
     </PageLayout>
   );

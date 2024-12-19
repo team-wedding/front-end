a33df2b9c6c2b58217e0f55624565d9a40d30689
Soyeon Lee <pink731p@gmail.com>
2024-12-19 21:04:25 +0900

    Feat: 결과 페이지 생성

---
+++ src/pages/ResultPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResultDisplay from '../components/display/ResultDisplay';
+
+const ResultPage = () => {
+  return (
+    <div className="rounded-none result-layout">
+      <ResultDisplay />
+    </div>
+  );
+};
+
+export default ResultPage;

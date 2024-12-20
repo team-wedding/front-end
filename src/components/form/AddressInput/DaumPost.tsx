1e6af6aff10a449aabb78efc9458b56123ea0918
Soyeon Lee <pink731p@gmail.com>
2024-12-20 22:13:42 +0900

    Feat: 지번 주소 추가

--- src/components/form/AddressInput/DaumPost.tsx
+++ src/components/form/AddressInput/DaumPost.tsx
@@ -4,6 +4,7 @@ import useAddressStore from '../../../store/useAddressStore';
 
 interface PostcodeData {
   address: string;
+  jibunAddress: string;
   zonecode: string;
   addressType: string;
   bname: string;
@@ -16,12 +17,15 @@ interface DaumPostProps {
 
 const DaumPost: React.FC<DaumPostProps> = ({ handleComplete }) => {
   const setAddress = useAddressStore((state) => state.setAddress);
+  const setJibunAddress = useAddressStore((state) => state.setJibunAddress);
 
   // DaumPostcode API 완료 시 실행되는 함수
   const complete = (data: PostcodeData) => {
     let fullAddress = data.address;
     let zonecode = data.zonecode;
     let extraAddress = '';
+
+    const jibunAddress = data.jibunAddress || '';
 
     if (data.addressType === 'R') {
       if (data.bname) {
@@ -41,6 +45,7 @@ const DaumPost: React.FC<DaumPostProps> = ({ handleComplete }) => {
 
     // 부모 상태 업데이트
     setAddress(fullAddress, zonecode);
+    setJibunAddress(jibunAddress);
 
     // 팝업 닫기
     handleComplete();
@@ -49,12 +54,14 @@ const DaumPost: React.FC<DaumPostProps> = ({ handleComplete }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50">
       {/* 팝업 컨테이너 */}
-      <div className="relative bg-white rounded-lg shadow-lg w-[500px]">
+      <div className="relative bg-white rounded-xl shadow-lg w-[500px]">
         {/* 헤더 */}
-        <div className="flex justify-between items-center px-4 py-3 bg-gray-800 rounded-t-lg">
-          <h1 className="text-white text-lg font-bold">예식 장소 주소 검색</h1>
+        <div className="flex justify-between items-center px-6 py-3 bg-gray-800 rounded-t-lg">
+          <h1 className="text-white font-semibold opacity-90">
+            예식 장소 주소 검색
+          </h1>
           <i
-            className="bx bx-x text-2xl text-white cursor-pointer hover:text-gray-300"
+            className="bx bx-x text-xl text-white cursor-pointer hover:text-gray-300"
             onClick={handleComplete}
           ></i>
         </div>
--- src/store/useAddressStore.ts
+++ src/store/useAddressStore.ts
@@ -2,21 +2,25 @@ import { create } from 'zustand';
 
 type AddressState = {
   address: string;
+  jibunAddress: string;
   zonecode: string | number;
   coords: { lat: number; lng: number };
   weddingHallName: string;
   weddingHallDetail: string;
   setAddress: (address: string, zonecode: string | number) => void;
+  setJibunAddress: (jibunAddress: string) => void;
   setCoords: (lat: number, lng: number) => void;
 };
 
 const useAddressStore = create<AddressState>((set) => ({
   address: '',
+  jibunAddress: '',
   zonecode: '',
   weddingHallName: '',
   weddingHallDetail: '',
   coords: { lat: 37.5665, lng: 126.978 },
   setAddress: (address, zonecode) => set(() => ({ address, zonecode })),
+  setJibunAddress: (jibunAddress) => set(() => ({ jibunAddress })),
   setCoords: (lat, lng) => set(() => ({ coords: { lat, lng } })),
 }));
 

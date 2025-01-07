d5d3a9b0bdecdc802e8c50283d0cda2bacd47cff
Soyeon Lee <pink731p@gmail.com>
2025-01-07 16:49:44 +0900

    Feat: display 폴더 구조 수정

---
+++ src/components/display/InformSection/InformSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InformSection = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      InformSection
+    </div>
+  );
+};
+
+export default InformSection;

71d3e52424cf7960611a51d2da618b053e497e75
Soyeon Lee <pink731p@gmail.com>
2025-01-07 22:11:31 +0900

    Feat

--- src/constants/accordionData.tsx
+++ src/constants/accordionData.tsx
@@ -11,6 +11,8 @@ import CalendarSelection from '../components/form/CalendarSelection/CalendarSele
 import MessageSelection from '../components/form/MessageSelection/MessageSelection';
 import AccountInput from '../components/form/AccountInput/AccountInput';
 import InformSection from '../components/display/InformSection/InformSection';
+import GallerySelection from '../components/form/GallerySelection/GallerySelection';
+import LocationSelection from '../components/form/LocationSelection/LocationSelection';
 
 export const accordionData: AccordionItemData[] = [
   {
@@ -50,46 +52,56 @@ export const accordionData: AccordionItemData[] = [
   },
   {
     id: 8,
+    title: '지도 / 교통수단',
+    content: <LocationSelection />,
+  },
+  {
+    id: 9,
+    title: '갤러리',
+    content: <GallerySelection />,
+  },
+  {
+    id: 10,
     title: '축하 메시지',
     content: <MessageSelection />,
   },
   {
-    id: 9,
+    id: 11,
     title: '축의금',
     content: <AccountInput />,
   },
   {
-    id: 10,
+    id: 12,
     title: '연락하기',
     content: <ContactInput />,
   },
   {
-    id: 11,
+    id: 13,
     title: '공지사항',
     content: <InformSection />,
   },
   {
-    id: 12,
+    id: 14,
     title: '실시간 포토월',
     content: <RsvpExample />,
   },
   {
-    id: 13,
+    id: 15,
     title: '테마 색상',
     content: <ThemeSelection />,
   },
   {
-    id: 14,
+    id: 16,
     title: '글꼴',
     content: <ThemeSelection />,
   },
   {
-    id: 15,
+    id: 17,
     title: '인트로 효과',
     content: <ThemeSelection />,
   },
   {
-    id: 16,
+    id: 18,
     title: '배경 음악',
     content: <ThemeSelection />,
   },

6dea5ac4c54fd4fa8f1756bf205f33030ce0c24a
Soyeon Lee <pink731p@gmail.com>
2025-01-15 03:44:47 +0900

    Feat: 토글 상태로 렌더링 가능하게 수정

--- src/components/display/CalendarSection/CalendarSection.tsx
+++ src/components/display/CalendarSection/CalendarSection.tsx
@@ -1,39 +1,42 @@
+import { useMainFeatureStore } from '../../../store/Feature/useMainFeatureStore';
+import { useCalendarFeatureStore } from '../../../store/Feature/useCalendarFeatureStore';
 import { useWeddingStore } from '../../../store/useWeddingStore';
 import Calendar from './Calendar';
 import CalendarTitle from './CalendarTitle';
 import CountDown from './CountDown';
 import DDay from './DDay';
-import { useCalendarStore } from '../../../store/useCalendarStore';
 
 const CalendarSection = () => {
   const { weddingDate, weddingTime } = useWeddingStore();
-  const selectedFeatures = useCalendarStore((state) => state.selectedFeatures);
+  const { selectedMainFeatures } = useMainFeatureStore();
+  const { subFeatures } = useCalendarFeatureStore();
 
-  const renderFeatures = () => {
-    return selectedFeatures.map((feature) => {
-      switch (feature) {
-        case 'calendar':
-          return (
-            <>
-              <CalendarTitle key={'calendar'} />
-              <Calendar key={'calendar'} />
-            </>
-          );
-        case 'dday':
-          return <DDay key="dday" />;
-        case 'countdown':
-          return (
-            <CountDown
-              key="countdown"
-              targetDate={weddingDate}
-              targetTime={weddingTime}
-            />
-          );
-      }
-    });
-  };
+  const isMainCalendarActive = selectedMainFeatures.mainCalendar;
 
-  return <div className="column-center gap-10">{renderFeatures()}</div>;
+  const renderSubFeatures = [
+    subFeatures.calendar && <Calendar key="calendar" />,
+    subFeatures.countdown && (
+      <CountDown
+        key="countdown"
+        targetDate={weddingDate}
+        targetTime={weddingTime}
+      />
+    ),
+    subFeatures.dday && <DDay key="dday" />,
+  ].filter(Boolean);
+
+  return (
+    isMainCalendarActive && (
+      <div className="column-center gap-6">
+        <div className="text-3xl font-thin tracking-widest">WEDDING DAY</div>
+        <CalendarTitle />
+        <div className="column-center gap-10">
+          {/* 캘린더 기능이 활성화 될때만 렌더링 */}
+          {renderSubFeatures}
+        </div>
+      </div>
+    )
+  );
 };
 
 export default CalendarSection;

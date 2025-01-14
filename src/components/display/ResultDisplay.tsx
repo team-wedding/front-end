be5f305141443c887885f0d64cb73d6ee9dc677d
hcy08 <sweet17strawberry@gmail.com>
2025-01-14 16:55:41 +0900

    Rename: 파일명 수정

--- src/components/display/ResultDisplay.tsx
+++ src/components/display/ResultDisplay.tsx
@@ -6,7 +6,6 @@ import GreetingSection from './GreetingSection/GreetingSection';
 import GallerySection from './GallerySection/GallerySection';
 import MoneySection from './MoneySection/MoneySection';
 import NoticeSection from './NoticeSection/NoticeSection';
-import NoticeSection from './NoticeSection/NoticeSection';
 import PhotoTalkSection from './PhotoTalkSection/PhotoTalkSection';
 import NoticeSection from './NoticeSection/NoticeSection';
 
@@ -20,6 +19,7 @@ const ResultDisplay = () => {
       <ContactSection />
       <GallerySection />
       <MoneySection />
+      <NoticeSection />
       <NoticeSection />
       <PhotoTalkSection />
     </div>

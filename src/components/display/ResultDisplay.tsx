1a12cf729098b12d0f9fa6c05c3ad6d2f1838a24
hcy08 <sweet17strawberry@gmail.com>
2025-01-14 16:55:41 +0900

    Rename: 파일명 수정

--- src/components/display/ResultDisplay.tsx
+++ src/components/display/ResultDisplay.tsx
@@ -5,8 +5,8 @@ import ThumbnailSection from './ThumbnailSection/ThumbnailSection';
 import GreetingSection from './GreetingSection/GreetingSection';
 import GallerySection from './GallerySection/GallerySection';
 import MoneySection from './MoneySection/MoneySection';
-import PhotoTalkSection from './PhotoTalkSection/PhotoTalkSection';
 import NoticeSection from './NoticeSection/NoticeSection';
+import PhotoTalkSection from './PhotoTalkSection/PhotoTalkSection';
 
 const ResultDisplay = () => {
   return (

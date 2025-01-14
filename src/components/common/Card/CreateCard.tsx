ef7f943419b916290ded60ff7cc66fbe2977b6a2
meteorqz6 <nys8765@naver.com>
2025-01-14 17:02:28 +0900

    Refactor: 모달 열기/닫기 로직 함수 개선

--- src/components/common/Card/CreateCard.tsx
+++ src/components/common/Card/CreateCard.tsx
@@ -4,27 +4,26 @@ import InputTitleModal from '../Modal/InputTitleModal';
 
 const CreateCard = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleModal = () => setIsModalOpen((prev) => !prev);
 
-  // 모달 열기
-  const handleOpenModal = () => setIsModalOpen(true);
+  let modal = null;
 
-  // 모달 닫기
-  const handleCloseModal = () => setIsModalOpen(false);
+  if (isModalOpen) {
+    modal = <InputTitleModal isOpen={isModalOpen} onClose={handleModal} />;
+  }
 
   return (
     <>
       <div
         className="column-center gap-2 outline-dotted outline-gray-400 rounded-lg w-[150px] h-[225px] bg-gray-100 font-light text-xs text-gray-500 opacity-60 cursor-pointer hover:bg-background hover:bg-opacity-50 hover:text-black hover:shadow-xl hover:outline-transparent transition-all duration-300 delay-100 ease-in-out"
-        onClick={handleOpenModal} // 버튼 클릭 시 모달 열기
+        onClick={handleModal}
       >
         <button>
           <CirclePlusIcon />
         </button>
         <div>청첩장 만들기</div>
       </div>
-
-      {/* 모달 */}
-      <InputTitleModal isOpen={isModalOpen} onClose={handleCloseModal} />
+      {modal}
     </>
   );
 };

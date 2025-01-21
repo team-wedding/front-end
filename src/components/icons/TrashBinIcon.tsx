8460ebdf6e346e6602d3e6d41f80094cc70b37c4
hcy08 <sweet17strawberry@gmail.com>
2025-01-21 10:03:58 +0900

    Feat

---
+++ src/components/icons/TrashBinIcon.tsx
@@ -0,0 +1,25 @@
+function TrashBinIcon() {
+  return (
+    <div>
+      <svg
+        className="w-[20px] h-[20px] text-gray-800 dark:text-white hover:text-red-500"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default TrashBinIcon;

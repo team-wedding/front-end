b51ebb4c3ac562c07818f37b3b9e6f0cc4ddcef6
robin <nowrobin3@gmail.com>
2025-01-22 14:07:43 +0900

    fix: 갤러리 이미지 사이즈 지정

--- src/components/form/Feature/GalleryFeature/GalleryFeature.tsx
+++ src/components/form/Feature/GalleryFeature/GalleryFeature.tsx
@@ -1,12 +1,13 @@
 import { useRef, useState } from 'react';
 import useGallaryStore from '../../../../store/useGallaryStore';
 import CloseIcon from '../../../icons/CloseIcon';
-import GridIcon from '../../../icons/GridIcon';
+import GridIcon from '../../../../assets/GridIcon.svg'
+import ChevronRight from '../../../icons/Chevron_RightIcon';
+import ChevronLeft from '../../../icons/Chevron_LeftIcon';
 
 export default function GalleryFeature() {
   const fileRef = useRef<HTMLInputElement>(null);
-  const [images, setImages] = useState<string[]>([]);
-  const [grid, setGrid] = useState(false);
+  const { images, grid, setImages, setGrid } = useGallaryStore();
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (images.length + e.target.files!.length <= 9) {
@@ -14,12 +15,12 @@ export default function GalleryFeature() {
       const fileArray = [...target].map((value: Blob) =>
         URL.createObjectURL(value),
       );
-      setImages((prev) => [...prev, ...fileArray]);
+      setImages([...images, ...fileArray]);
     } else alert('9 images?');
   };
 
   const handleDelete = (index: number) => {
-    setImages((prev) => prev.filter((_, i) => i !== index));
+    setImages(images.filter((_, i) => i !== index));
   };
 
   const handleDrag = (e: React.DragEvent<HTMLLabelElement>) => {
@@ -41,7 +42,7 @@ export default function GalleryFeature() {
         if (file) {
           reader.readAsDataURL(file);
           reader.onload = () => {
-            setImages((prev) => [...prev, reader.result as string]);
+            setImages([...images, reader.result as string])
           };
         }
       }
@@ -77,16 +78,11 @@ export default function GalleryFeature() {
     setHoveredIndex(null);
   };
 
-  const store = useGallaryStore();
-  const handleSave = () => {
-    store.setImages(images);
-    store.setGrid(grid);
-  };
 
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-4">
       <label
-        className={`${images.length == 0 ? `flex flex-col` : `grid grid-cols-3 gap-1`} w-full h-fit p-1 border-2  rounded-md cursor-pointer items-center`}
+        className={`${images.length === 0 ? `flex flex-col` : `grid grid-cols-2 justify-items-center gap-2 px-4 lg:px-1 py-2 lg:grid-cols-3 lg:gap-1`} w-full h-fit p-1 border-2  rounded-md cursor-pointer items-center overflow-y-scroll`}
         htmlFor="dropzone"
         onDrop={handleDrop}
         onDragOver={handleDrag}
@@ -94,7 +90,7 @@ export default function GalleryFeature() {
         onDragEnter={handleDragpEnter}
         onClick={(e) => e.preventDefault()}
       >
-        {images.length == 0 ? (
+        {images.length === 0 ? (
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
             <p className="mb-2 text-xs text-gray-500">
               <span className="font-semibold">Click to upload</span> or drag and
@@ -108,7 +104,7 @@ export default function GalleryFeature() {
           images.map((value, index) => {
             return (
               <div
-                className={`relative w-24 h-32 border border-gray-300 rounded-md flex items-center justify-center cursor-pointer ${hoveredIndex == index ? 'shadow-2xl' : 'shadow-none '}`}
+                className={`relative w-32 h-44 lg:w-24 lg:h-32 rounded-md flex items-center justify-center cursor-pointer ${hoveredIndex == index ? 'shadow-2xl' : 'shadow-none '}`}
                 key={index}
                 onDragStart={() => handleSortDragStart(index)}
                 onDragOver={(e) => handleSortDragOver(index, e)}
@@ -116,12 +112,12 @@ export default function GalleryFeature() {
                 onDragEnd={handleSortDragEnd}
               >
                 <div
-                  className="absolute size-4 top-1 right-1 rounded-full bg-gray-400/80"
+                  className="absolute size-4 top-2 right-2 rounded-full bg-gray-400/60 group hover:bg-black"
                   onClick={() => handleDelete(index)}
                 >
                   <CloseIcon className="size-4 text-white " />
                 </div>
-                <img src={value} alt="" className="" />
+                <img src={value} alt="" className="object-center  rounded-md size-full border border-gray-300" />
               </div>
             );
           })
@@ -142,28 +138,32 @@ export default function GalleryFeature() {
       >
         사진 추가하기
       </label>
-      <section className="w-full flex justify-around">
+      <section className="w-full flex flex-row items-stretch gap-4 h-28">
         <button
           onClick={() => setGrid(true)}
-          className={`px-10 py-2 rounded-md ${grid ? 'bg-gray-200 border border-black ' : 'bg-transparent  '}`}
+          className={`flex-1 px-5 py-4 rounded-md flex flex-col items-center justify-between border ${grid ? 'border-transparent ring-1 ring-primary' : 'bg-transparent  '}`}
         >
-          <GridIcon />
+          <img src={GridIcon} alt="" className='size-12 text-gray-400' />
           그리드
         </button>
         <button
           onClick={() => setGrid(false)}
-          className={`px-10 py-2 rounded-md ${!grid ? 'bg-gray-200 border border-black ' : 'bg-transparent  '}`}
+          className={` flex-1 px-5 py-4 rounded-md flex flex-col items-center justify-between border  ${!grid ? 'border-transparent ring-1 ring-primary' : 'bg-transparent  '}`}
         >
-          <GridIcon />
+          <div className=' flex flex-row items-center'>
+            <ChevronLeft className='text-gray-400/70 size-6' />
+            <div className='size-12 bg-gray-400/70 rounded-lg' />
+            <ChevronRight className='text-gray-400/70 size-6' />
+          </div>
           페이징
         </button>
       </section>
-      <button
+      {/* <button
         onClick={handleSave}
         className="bg-button p-2 rounded-md text-white hover:bg-button/80"
       >
         저장하기
-      </button>
-    </div>
+      </button> */}
+    </div >
   );
 }

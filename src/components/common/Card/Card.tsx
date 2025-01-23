cb091a1d47b7756745e5506d4028fb883ad66d0c
robin <nowrobin3@gmail.com>
2025-01-23 13:51:14 +0900

    refact: code cleaner

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -13,7 +13,7 @@ const Card: React.FC<CardProps> = ({ image, id, title }) => {
     <div className="relative flex flex-col items-center border border-gray-200 rounded-lg shadow-xl w-[150px] h-[225px] bg-gray-200">
       <div className="relative h-full w-full rounded-lg overflow-hidden">
         <img
-          src={image == "" ? logo : image}
+          src={image || logo}
           className="object-cover h-full w-[188px] rounded-lg"
         ></img>
         <div className="absolute inset-0 bg-white opacity-10"></div>

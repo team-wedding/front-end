63d452d20489fc0a2a09830ca2ba72d35d13a5e8
robin <nowrobin3@gmail.com>
2025-01-23 20:53:52 +0900

    feat: 축의금 미리보기

--- src/components/display/MoneySection/MoneySection.tsx
+++ src/components/display/MoneySection/MoneySection.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import { useState } from 'react';
+import AccountNumberItem from '../../common/AccountNumberItem/AccountNumberItem';
+import useAccountStore from '../../../store/useAccountStore';
 
 const MoneySection = () => {
-  return <div className="column-center">MoneySection</div>;
+  const { accounts } = useAccountStore();
+  const [accordionStates, setAccordionStates] = useState({
+    groom: false,
+    bride: false,
+  });
+  const toggleAccordion = (key: "groom" | "bride") => {
+    setAccordionStates((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
+  return (
+    <div className="column-center w-full flex flex-col gap-4 h-fit">
+      <div className='font-semibold text-xl'>마음 전하실곳 </div>
+      {
+        accounts.map((value, index) => {
+          const { accountInfo, motherAccountInfo, fatherAccountInfo } = value
+          const isOpen = value.role === "신랑님" ? accordionStates.groom : accordionStates.bride
+          return (
+            <section className="flex flex-col w-72" key={index}>
+              <div
+                className={`flex py-2 px-5 cursor-default justify-between items-center bg-secondary rounded-lg ${isOpen && "rounded-b-none"
+                  }`}
+                onClick={() => toggleAccordion(value.role === "신랑님" ? "groom" : "bride")}
+              >
+                <div className="font-medium">{`${value.role} 계좌번호`}</div>
+                <i
+                  className={`bx bx-chevron-down text-xl transition-all duration-300 ${isOpen ? "rotate-180" : ""
+                    }`}
+                ></i>
+              </div>
+              <div
+                className={`overflow-hidden transition-all ${isOpen ? "h-fit" : "h-0"
+                  }`}
+              >
+                <AccountNumberItem
+                  bank={accountInfo.bankName}
+                  accountNumber={accountInfo.accountNumber}
+                  name={accountInfo.accountHolder}
+                  kakaoLink={accountInfo.kakaoPayQRCode}
+                />
+                <AccountNumberItem
+                  bank={fatherAccountInfo.bankName}
+                  accountNumber={fatherAccountInfo.accountNumber}
+                  name={fatherAccountInfo.accountHolder}
+
+                  kakaoLink={fatherAccountInfo.kakaoPayQRCode}
+                />
+                <AccountNumberItem
+                  bank={motherAccountInfo.bankName}
+                  accountNumber={motherAccountInfo.accountNumber}
+                  name={motherAccountInfo.accountHolder}
+                  kakaoLink={motherAccountInfo.kakaoPayQRCode}
+                  last={true}
+                />
+              </div>
+            </section>
+          )
+        })
+      }
+    </div >
+  )
 };
 
 export default MoneySection;

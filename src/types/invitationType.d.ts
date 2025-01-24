bb0bca88e267eade1a117c49c515c43e84af26e8
robin <nowrobin3@gmail.com>
2025-01-24 16:31:58 +0900

    a

--- src/types/invitationType.d.ts
+++ src/types/invitationType.d.ts
@@ -1,16 +1,18 @@
 export interface InvitationDetiail {
   id?: number;
-  createdAt?: string;
-  updatedAt?: string;
+  title: string;
+  // createdAt?: string;
+  // updatedAt?: string;
   groomName: string;
   brideName: string;
   groomContact: string;
   brideContact: string;
   date: string;
-  location: string;
+  location: string[];
   imgUrl: string;
-  contentType: '제목';
+  contentType: string;
   content: string;
+  weddingTime: string;
   groomFatherName: string;
   groomMotherName: string;
   brideFatherName: string;
@@ -19,18 +21,90 @@ export interface InvitationDetiail {
   groomMotherContact: string;
   brideFatherContact: string;
   brideMotherContact: string;
-  weddingTime: string;
   groomFatherAlive: boolean;
   groomMotherAlive: boolean;
   brideFatherAlive: boolean;
   brideMotherAlive: boolean;
-  font: string;
-  weight: string;
   backgroundColor: string;
+  attendanceTitle: '참석 여부 제목';
+  attendanceContent: '참석 여부 설명';
+  attendanceIsDining: true;
+  font: string;
+  calendars?: CalaendarDetail[];
+  maps?: MapDetail[];
+  galleries?: [];
+  accounts?: [
+    {
+      order: number;
+      accountHolderName: string;
+      bankName: string;
+      accountNumber: string;
+      kakaoUrl: string;
+    },
+  ];
+  contacts?: [
+    {
+      order: number;
+      groomContact: string;
+      brideContact: string;
+      groomFatherContact: string;
+      groomMotherContact: string;
+      brideFatherContact: string;
+      brideMotherContact: string;
+    },
+  ];
+  notices?: [
+    {
+      order: number;
+      title: string;
+      content: string;
+      image: string;
+    },
+  ];
 }
 
-interface ContactInfo {
-  name: string;
-  contact: string;
-  alive?: boolean;
+interface CalaendarDetail {
+  order: number;
+  calendar: true;
+  dDay: true;
+  countdown: true;
+}
+interface MapDetail {
+  order: number;
+  tMap: true;
+  naverMap: false;
+  kakaoMap: false;
+  personalCar: true;
+  subway: false;
+  bus: false;
+  personalCarContent: string;
+  subwayContent: string;
+  busContent: string;
+}
+interface GalleryDetail {
+  order: number;
+  images: string[];
+  grid: true;
+}
+interface AccountDetail {
+  order: number;
+  accountHolderName: string;
+  bankName: string;
+  accountNumber: string;
+  kakaoUrl: string;
+}
+interface ContactDetail {
+  order: number;
+  groomContact: string;
+  brideContact: string;
+  groomFatherContact: string;
+  groomMotherContact: string;
+  brideFatherContact: string;
+  brideMotherContact: string;
+}
+interface NoticeDetail {
+  order: number;
+  title: string;
+  content: string;
+  image: string;
 }

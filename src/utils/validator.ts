48a3ae363b11ff9593152c87ba68980f530ee4fe
meteorqz6 <nys8765@naver.com>
2025-01-30 13:39:32 +0900

    Refactor: 회원가입 코드 파일 분리

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,4 +1,14 @@
+export const validateName = (name: string): boolean => {
+  return name.length >= 2;
+}
+
 export const validateEmail = (email: string): boolean => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
+
+// 비밀번호 유효성 검사 (숫자, 영문, 특수문자 포함 8 ~ 16자)
+export const validatePassword = (password: string): boolean => {
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/;
+  return passwordRegex.test(password);
+}

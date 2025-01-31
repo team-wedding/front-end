a06ffb5515385290afe14be01fac1c9dcae161af
meteorqz6 <nys8765@naver.com>
2025-01-31 21:46:24 +0900

    Feat: 신랑, 신부 이름 입력 유효성 검사 기능 추가

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -2,3 +2,7 @@ export const validateEmail = (email: string): boolean => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
+
+export const validateBrideGroomNames = (brideGroom: { name: string }[]) => {
+  return brideGroom.every((person) => person.name.trim() !== '');
+}

d72e5dada754eab264eab9ae2f03fdd23d6eec18
meteorqz6 <nys8765@naver.com>
2025-01-31 20:13:24 +0900

    Feat: props에 navigate href 추가

--- src/components/common/MyPage/MyPageItem.tsx
+++ src/components/common/MyPage/MyPageItem.tsx
@@ -1,16 +1,23 @@
+import { useNavigate } from "react-router";
+
 interface Props {
   icon: string;
   title: string;
   detail: string;
+  href: string;
 }
 
-export default function MyPageItem({ icon, title, detail }: Props) {
+export default function MyPageItem({ icon, title, detail, href }: Props) {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-row border  border-black rounded-md px-2 py-4 items-center justify-around">
+    <div
+      className="flex flex-row border border-black rounded-md px-2 py-4 items-center justify-around cursor-pointer"
+      onClick={() => navigate(href)}>
       <img src={icon} alt="icon" />
-      <div className="flex flex-col">
-        <div>{title}</div>
-        <div>{detail}</div>
+      <div className="flex flex-col gap-2">
+        <div className="text-base">{title}</div>
+        <div className="text-xs">{detail}</div>
       </div>
     </div>
   );

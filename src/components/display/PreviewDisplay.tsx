426b19ab9d8b18e879bc1449e0cbe7294abe9e30
robin <nowrobin3@gmail.com>
2025-02-04 12:50:56 +0900

    feat: Add Font assets

--- src/components/display/PreviewDisplay.tsx
+++ src/components/display/PreviewDisplay.tsx
@@ -1,10 +1,11 @@
+import useThemeStore from '@/store/useThemeStore';
 import { useAccordionStore } from '@store/useAccordionStore';
 
 const PreviewDisplay = () => {
   const { getSections } = useAccordionStore();
-
+  const { font } = useThemeStore()
   return (
-    <div className="result-layout">
+    <div className={`result-layout font-${font}`}>
       {getSections().map((section, index) => (
         <div key={index}>{section}</div>
       ))}
--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -2,20 +2,23 @@ import { create } from 'zustand';
 
 interface ThemeState {
   font: string;
+  fontIndex: number;
   bigSize: boolean;
-  setFont: (font: string) => void;
+  setFont: (font: string, fontIndex: number) => void;
   updateSize: (size: boolean) => void;
   reset: () => void;
 }
 
 const initialState = {
-  font: '',
+  font: 'Mapo',
+  fontIndex: 0,
   bigSize: false,
 };
 
 const useThemeStore = create<ThemeState>((set) => ({
   ...initialState,
-  setFont: (font: string) => set(() => ({ font: font })),
+  setFont: (font: string, fontIndex: number) =>
+    set(() => ({ font, fontIndex })),
   updateSize: (bigSize: boolean) => set(() => ({ bigSize: bigSize })),
   reset: () => set(() => initialState),
 }));

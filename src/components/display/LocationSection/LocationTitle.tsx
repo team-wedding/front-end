000f8d4a8188eccc4d56055225e4f2fa8e505669
meteorqz6 <nys8765@naver.com>
2025-02-05 15:08:03 +0900

    Feat: 공통 컴포넌트 Toast 추가

--- src/components/display/LocationSection/LocationTitle.tsx
+++ src/components/display/LocationSection/LocationTitle.tsx
@@ -1,8 +1,10 @@
 import useAddressStore from '@store/useAddressStore';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import FileCopyIcon from '@icons/FileCopyIcon';
-import { toast, ToastContainer } from 'react-toastify';
+// import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ToastPopup from '@/components/common/Toastpopup';
+import { useState } from 'react';
 
 const LocationTitle = () => {
   // 웨딩 이름, 층, 홀 추가하기
@@ -10,12 +12,15 @@ const LocationTitle = () => {
 
   const addressToCopy = jibunAddress || '서울 강남구 언주로 564';
 
+  const [toast, setToast] = useState(false);
+
   const handleCopy = () => {
-    toast.success('클립보드에 복사되었습니다 😀', {
-      position: 'bottom-center',
-      autoClose: 2000,
-      hideProgressBar: true,
-    });
+    // toast.success('클립보드에 복사되었어요!', {
+    //   position: 'bottom-center',
+    //   autoClose: 2000,
+    //   hideProgressBar: true,
+    // });
+    setToast(true);
   };
 
   return (
@@ -31,7 +36,14 @@ const LocationTitle = () => {
           </button>
         </CopyToClipboard>
       </div>
-      <ToastContainer />
+      {/* <ToastContainer /> */}
+      {toast && (
+        <ToastPopup
+          setToast={setToast}
+          message={'클립보드에 복사되었어요.'}
+          position="bottom"
+        />
+      )}
     </div>
   );
 };

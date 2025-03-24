8221dc4a34560fd0b8620ff80ba8fbb294338549
hcy08 <sweet17strawberry@gmail.com>
2025-03-24 14:27:06 +0900

    feat: 다크모드 provider 추가

--- src/components/common/DarkMode/DarkModeToggle.tsx
+++ src/components/common/DarkMode/DarkModeToggle.tsx
@@ -1,11 +1,9 @@
-import useDarkMode from '@/hooks/useDarkMode';
 import { useDarkModeStore } from '@/store/useDarkModeStore';
 import MoonIcon from '@/components/icons/MoonIcon';
 import SunIcon from '@/components/icons/SunIcon';
 
 const DarkModeToggle = () => {
   const { mode, setMode } = useDarkModeStore();
-  useDarkMode();
 
   const toggleMode = () => {
     const newMode = mode === 'light' ? 'dark' : 'light';
@@ -13,14 +11,12 @@ const DarkModeToggle = () => {
   };
 
   return (
-    <div className="m-4">
-      <button
-        onClick={toggleMode}
-        className="p-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        {mode === 'light' ? <MoonIcon /> : <SunIcon />}
-      </button>
-    </div>
+    <button
+      onClick={toggleMode}
+      className="p-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-700"
+    >
+      {mode === 'light' ? <MoonIcon /> : <SunIcon />}
+    </button>
   );
 };
 

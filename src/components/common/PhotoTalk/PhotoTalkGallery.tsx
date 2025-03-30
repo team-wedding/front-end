0db5e0b7d61bbba99a0b1b96885694d57e571da4
Soyeon Lee <pink731p@gmail.com>
2025-03-30 16:36:06 +0900

    Feat: 포토톡 갤러리 컴포넌트

--- src/components/common/PhotoTalk/PhotoTalkGallery.tsx
+++ src/components/common/PhotoTalk/PhotoTalkGallery.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import usePhotoTalkStore from '@store/usePhotoTalkStore';
-import ChevronLeft from '@icons/Chevron_LeftIcon';
-import ChevronRight from '@icons/Chevron_RightIcon';
 import DownloadIcon from '@/components/icons/DownloadIcon';
-import JSZip from 'jszip';
-import { saveAs } from 'file-saver';
+import { downloadSelectedImages } from '@/utils/downloadUtils';
+import PhotoTalkGalleryModal from '@/components/common/PhotoTalk/PhotoTalkGalleryModal';
 
 interface PhotoTalkGalleryProps {
   isAdmin?: boolean;
@@ -17,8 +15,6 @@ const PhotoTalkGallery = ({ isAdmin = false }: PhotoTalkGalleryProps) => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
-
   const toggleSelectImage = (url: string) => {
     setSelectedImages((prevSelected) =>
       prevSelected.includes(url)
@@ -27,66 +23,16 @@ const PhotoTalkGallery = ({ isAdmin = false }: PhotoTalkGalleryProps) => {
     );
   };
 
-  const downloadSelectedImages = async (selectedImages: string[]) => {
-    // zip 다운
-    if (selectedImages.length >= 10) {
-      const zip = new JSZip();
-      const folder = zip.folder('phototalk-images');
-
-      await Promise.all(
-        selectedImages.map(async (url, index) => {
-          const response = await fetch(url);
-          const blob = await response.blob();
-          folder?.file(`phototalk_${index + 1}.jpg`, blob);
-        }),
-      );
-
-      const content = await zip.generateAsync({ type: 'blob' });
-      saveAs(content, 'phototalk_images.zip');
-    } else {
-      // 순차 다운
-      for (let i = 0; i < selectedImages.length; i++) {
-        const url = selectedImages[i];
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = `phototalk_${i + 1}.jpg`;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-
-        await delay(500);
-      }
-    }
-
-    console.log(`${selectedImages.length} images downloaded`);
-  };
-
-  const downloadCurrentImage = () => {
-    const url = images[currentImageIndex];
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = `phototalk_${currentImageIndex + 1}.jpg`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  // 여러 장 다운로드
+  const handleDownloadSelected = async () => {
+    await downloadSelectedImages(selectedImages);
   };
 
   const openModal = (index: number) => {
     setCurrentImageIndex(index);
     setModalOpen(true);
   };
-
   const closeModal = () => setModalOpen(false);
-
-  const showNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const showPreviousImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1,
-    );
-  };
 
   if (images.length === 0) {
     return (
@@ -99,98 +45,58 @@ const PhotoTalkGallery = ({ isAdmin = false }: PhotoTalkGalleryProps) => {
   return (
     <div>
       {isAdmin && (
-        <div className="flex justify-end items-center px-2">
-          {/* <h2 className="text-xs text-black/50">사진을 관리할 수 있습니다.</h2> */}
+        <header className="flex justify-end items-center px-2" role="banner">
           <div className="flex-center gap-2 mb-2">
             <p className="text-gray-700 text-xs">
               {selectedImages.length} / {images.length}
             </p>
             <button
-              onClick={() => downloadSelectedImages(selectedImages)}
+              onClick={handleDownloadSelected}
               className="bg-white/80 px-2 py-1 rounded-xl active:bg-black/30 shadow-md"
               disabled={selectedImages.length === 0}
+              aria-label="선택한 이미지 다운로드"
             >
               <DownloadIcon />
             </button>
           </div>
-        </div>
+        </header>
       )}
 
-      <div className="grid grid-cols-3 gap-[2px] p-2 place-items-center">
+      <main
+        className="grid grid-cols-3 gap-[2px] p-2 place-items-center"
+        role="main"
+      >
         {images.map((url, index) => (
-          <div key={index} className="relative group">
+          <div key={index} className="relative group hover:opacity-95">
             {isAdmin && (
               <div className="absolute -top-1 left-0 p-1 rounded-sm">
                 <input
                   type="checkbox"
                   checked={selectedImages.includes(url)}
                   onChange={() => toggleSelectImage(url)}
-                  className="size-4 rounded bg-white/60 cursor-pointer z-10 border-none shadow-inner checked:bg-black focus:ring-0 focus:outline-none"
+                  className="size-4 rounded bg-white/60  cursor-pointer z-10 border-none shadow-inner checked:bg-black focus:ring-0 focus:outline-none"
+                  aria-label={`이미지 ${index + 1} 선택`}
                 />
               </div>
             )}
 
             <img
               src={url}
-              alt={`Uploaded ${index}`}
+              alt={`Uploaded image ${index + 1}`}
               className="w-full aspect-[1/1] rounded-sm object-cover cursor-pointer shadow-custom"
               onClick={() => openModal(index)}
             />
           </div>
         ))}
-      </div>
+      </main>
 
       {isModalOpen && (
-        <div
-          onClick={closeModal}
-          className="max-w-[520px] m-auto column-center fixed inset-0 z-50 bg-black bg-opacity-80"
-        >
-          <div
-            onClick={(e) => e.stopPropagation()}
-            className="relative w-[90%] aspect-[4/5] rounded-2xl overflow-hidden backdrop-blur-3xl bg-black/30 shadow-custom py-16 "
-          >
-            <div className="relative flex-center w-full h-full px-4">
-              <button
-                onClick={showPreviousImage}
-                className="absolute left-3 top-1/2 transform -translate-y-1/2 rounded-full hover:opacity-50"
-              >
-                <ChevronLeft className="size-7 text-white/70" />
-              </button>
-
-              {/* <div className="p-10"> */}
-              <img
-                src={images[currentImageIndex]}
-                alt="미리보기"
-                className="max-w-full max-h-full object-contain rounded-xl"
-              />
-              {/* </div> */}
-
-              <button
-                onClick={showNextImage}
-                className="absolute right-3 top-1/2 transform -translate-y-1/2rounded-full hover:opacity-50"
-              >
-                <ChevronRight className="size-7 text-white/70" />
-              </button>
-            </div>
-
-            {isAdmin && (
-              <footer className="absolute bottom-3 right-0 left-0 m-auto w-full gap-4 text-white/80 px-4">
-                <button
-                  className="w-1/2 py-3 text-xs rounded-xl bg-black/0 hover:bg-white/10 font-medium"
-                  onClick={() => {}}
-                >
-                  삭제
-                </button>
-                <button
-                  className="w-1/2 py-3 text-xs rounded-xl bg-black/0 hover:bg-white/10 font-medium"
-                  onClick={downloadCurrentImage}
-                >
-                  다운로드
-                </button>
-              </footer>
-            )}
-          </div>
-        </div>
+        <PhotoTalkGalleryModal
+          isAdmin={isAdmin}
+          images={images}
+          currentImageIndex={currentImageIndex}
+          closeModal={closeModal}
+        />
       )}
     </div>
   );

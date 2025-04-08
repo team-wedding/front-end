35193af4e8a3fd3a755b3a802f3a117db3ee1f7a
Soyeon Lee <pink731p@gmail.com>
2025-04-08 20:22:28 +0900

    Feat: user 에 따른 포토톡 리스트 컴포넌트 분리

---
+++ src/components/common/PhotoTalk/List/PhotoTalkListAdmin.tsx
@@ -0,0 +1,60 @@
+import ReusableModal from '@/components/common/Modal/ReusableModal';
+import PhotoTalkCommonList from '@/components/common/PhotoTalk/List/PhotoTalkCommonList';
+import { PhotoTalk } from '@/types/phototalkType';
+import { useState } from 'react';
+import { usePhototalkAction } from '@/hooks/usePhototalkAction';
+import { ACTION_MODE, USER_MODE } from '@/types/users';
+import PhotoTalkListHeader from '@/components/common/PhotoTalk/List/PhotoTalkListHeader';
+
+const userMode = USER_MODE.ADMIN;
+
+interface PhotoTalkListAdminProps {
+  photoTalkList: PhotoTalk[];
+  isLoading: boolean;
+}
+
+const PhotoTalkListAdmin = ({
+  photoTalkList,
+  isLoading,
+}: PhotoTalkListAdminProps) => {
+  const [isGalleryOpen, setGalleryOpen] = useState(false);
+
+  const { isModalOpen, openModal, closeModal, confirmAction } =
+    usePhototalkAction({
+      mode: userMode,
+      onDelete: (photoTalk) => {
+        console.log(`${userMode}: ${photoTalk.name}님의 포토톡 삭제 완료`);
+      },
+    });
+
+  return (
+    <div>
+      <PhotoTalkListHeader
+        userMode={userMode}
+        isGalleryOpen={isGalleryOpen}
+        onToggleGallery={() => setGalleryOpen(!isGalleryOpen)}
+      />
+
+      {isLoading ? (
+        <>loading...</>
+      ) : (
+        <PhotoTalkCommonList
+          userMode={userMode}
+          photoTalkList={photoTalkList}
+          onDelete={(talk) => openModal(talk, ACTION_MODE.DELETE)}
+          isGalleryOpen={isGalleryOpen}
+        />
+      )}
+
+      <ReusableModal
+        isOpen={isModalOpen}
+        onCancel={closeModal}
+        onConfirm={confirmAction}
+        title="정말 삭제하시겠습니까?"
+        confirmText="삭제"
+      />
+    </div>
+  );
+};
+
+export default PhotoTalkListAdmin;

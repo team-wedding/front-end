eb7b3560780a0343051415b55c7e048fd09355a9
hcy08 <sweet17strawberry@gmail.com>
2025-04-16 10:37:26 +0900

    Design: 다크모드 클래스 추가

--- src/components/common/Card/CreateCardButton.tsx
+++ src/components/common/Card/CreateCardButton.tsx
@@ -1,11 +1,13 @@
-import logo from '@assets/woogyeol/logo_dark.png';
+import logo from '@assets/woogyeol/logo_light.png';
+import logoDark from '@assets/woogyeol/logo_dark.png';
 import PlusIcon from '@/components/icons/PlusIcon';
 
 const CreateCardButton = () => {
   return (
     <div className="flex-center">
       <button className="flex-center text-white  transition-all duration-200 w-8 hover:-translate-y-0.5 hover:w-9">
-        <img src={logo} className="relative" />
+        <img src={logo} className="relative block dark:hidden" />
+        <img src={logoDark} className="relative hidden dark:block" />
         <div className="absolute">
           <PlusIcon />
         </div>
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,16 +1,26 @@
 import Signup from '@/components/Signup';
 import logo from '@/assets/woogyeol/logo_light.png';
+import logoDark from '@/assets/woogyeol/logo_dark.png';
 
 const SignUpPage = () => {
   // const navigate = useNavigate();
 
   return (
-    <div className="bg-white column-center min-h-screen relative max-w-[520px] m-auto">
+    <div className="bg-surface dark:bg-surface-dark column-center min-h-screen relative max-w-[520px] m-auto">
       <header className="column-center mb-10">
         <div className="pt-6 pb-3">
-          <img src={logo} alt="우결 로고" className="w-20 opacity-80" />
+          <img
+            src={logo}
+            alt="우결 로고"
+            className="w-20 opacity-80 block dark:hidden"
+          />
+          <img
+            src={logoDark}
+            alt="우결 로고"
+            className="w-20 opacity-80 dark:block hidden"
+          />
         </div>
-        <span className="text-black/60 text-sm font-extralight">
+        <span className="text-label-secondary/60 dark:text-label-secondary-dark/60 text-sm font-extralight">
           우리만의 청첩장을 만들어봐요!
         </span>
       </header>

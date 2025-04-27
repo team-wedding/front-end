79877ed39a78149b033b435d4edfa7926bce499c
hcy08 <sweet17strawberry@gmail.com>
2025-04-27 13:40:12 +0900

    Feat: 스켈레톤 UI 추가

--- src/pages/DashBoardPage.tsx
+++ src/pages/DashBoardPage.tsx
@@ -2,9 +2,10 @@ import Card from '@common/Card/Card';
 import { InvitationDetiail } from '../types/invitationType';
 import { useEffect, useState } from 'react';
 import { useGetInvitations } from '@/hooks/useInvitation';
-import Navbar from '@/components/common/Navbar';
+import Navbar from '@common/Navbar/Navbar';
 import logo from '@/assets/woogyeol/logo_light.png';
 import DarkModeToggle from '@/components/common/DarkMode/DarkModeToggle';
+import SkeletonCard from '@/components/common/Skeleton/SkeletonCard';
 
 const DashBoardPage = () => {
   const { data, isPending, isRefetching, status, isError, isFetching } =
@@ -45,21 +46,21 @@ const DashBoardPage = () => {
           </div>
 
           <div className="grid grid-cols-2 gap-2 place-items-center">
-            {isPending || isRefetching ? (
-              <>Loading....</>
-            ) : (
-              invitations.map((card: InvitationDetiail) => (
-                <Card
-                  key={card.id}
-                  image={card.imgUrl}
-                  id={card.id as number}
-                  title={card.title}
-                />
-              ))
-            )}
+            {isPending || isRefetching
+              ? Array.from({ length: 4 }).map((_, i) => (
+                  <SkeletonCard key={i} />
+                ))
+              : invitations.map((card: InvitationDetiail) => (
+                  <Card
+                    key={card.id}
+                    image={card.imgUrl}
+                    id={card.id as number}
+                    title={card.title}
+                  />
+                ))}
           </div>
 
-          {invitations.length === 0 && (
+          {!isPending && !isRefetching && invitations.length === 0 && (
             <div className="column-center gap-4 w-3/4 m-auto h-60 rounded-2xl bg-black/20 text-white">
               <span className="text-md">아직 저장된 청첩장이 없어요.</span>
               <button className="w-28 py-2 text-sm bg-white/20 rounded-2xl trasition-all duration-200 hover:bg-black/10">

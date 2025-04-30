1addfa544a37842b64d5d5242a02f28af591d9cf
hcy08 <sweet17strawberry@gmail.com>
2025-04-30 17:42:25 +0900

    Feat: 로고 컴포넌트 추가

--- src/components/common/Card/CreateCardButton.tsx
+++ src/components/common/Card/CreateCardButton.tsx
@@ -1,13 +1,11 @@
-import logo from '@assets/woogyeol/logo_light.png';
-import logoDark from '@assets/woogyeol/logo_dark.png';
 import PlusIcon from '@/components/icons/PlusIcon';
+import Logo from '@common/Logo';
 
 const CreateCardButton = () => {
   return (
     <div className="flex-center">
       <button className="flex-center text-white  transition-all duration-200 w-8 hover:-translate-y-0.5 hover:w-9">
-        <img src={logo} className="relative block dark:hidden" />
-        <img src={logoDark} className="relative hidden dark:block" />
+        <Logo className="relative" />
         <div className="absolute">
           <PlusIcon />
         </div>

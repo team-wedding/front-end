3510a0eba4f85d807b6df2de141afc2ecf4dee58
hcy08 <sweet17strawberry@gmail.com>
2025-06-23 17:14:25 +0900

    feat: storybook 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,35 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import ts from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import react from 'eslint-plugin-react';
 import prettier from 'eslint-config-prettier';
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
-export default [
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': ts,
-      react,
-    },
-    rules: {
-      ...ts.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off', // JSX Transform 설정
-      'react/prop-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
-    },
-    settings: {
-      react: {
-        version: 'detect', // React 버전 자동 감지
-      },
+export default [{
+  files: ['**/*.{js,jsx,ts,tsx}'],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
     },
   },
-  prettier,
-];
+  plugins: {
+    '@typescript-eslint': ts,
+    react,
+  },
+  rules: {
+    ...ts.configs.recommended.rules,
+    ...react.configs.recommended.rules,
+    'react/react-in-jsx-scope': 'off', // JSX Transform 설정
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect', // React 버전 자동 감지
+    },
+  },
+}, prettier, ...storybook.configs["flat/recommended"]];

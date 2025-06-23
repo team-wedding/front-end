f36e018b959f593ff70b81c1c9f69497ff8c7cfe
hcy08 <sweet17strawberry@gmail.com>
2025-06-23 21:44:43 +0900

    feat: css 연결 및 배경 색상 설정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,15 @@
 import type { Preview } from '@storybook/react-vite';
+import '../src/styles/global.css';
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'light',
+      values: [
+        { name: 'light', value: '#ffffff' },
+        { name: 'dark', value: '#333333' },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
